33b3717c3ebf80a29582584f44cbf7e6e9471840
U-IBPC\IB <IB@IBPC.(none)>
2012-06-25 21:22:40 +0800

    initial deployment

---
+++ wojilu.cms/Controller/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using wojilu.Web;
+using wojilu.Web.Mvc;
+using wojilu.cms.Domain;
+
+namespace wojilu.cms.Controller {
+
+    public class AboutController : ControllerBase {
+
+        public void Index(  ) {
+           
+        }       
+
+    }
+
+}
---
+++ wojilu.cms/Controller/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using wojilu.Web.Mvc;
+using wojilu.cms.Domain;
+using wojilu.Web;
+
+namespace wojilu.cms.Controller {
+
+    public class ContactController : ControllerBase {
+
+        public ContactController() {
+            
+        }
+
+        public void Index()
+        {
+
+        }       
+
+    }
+
+}
